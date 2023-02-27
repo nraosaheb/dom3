b92f8e06dc38d480238684e4e837eed997dddd1f
Nikhil Kamdi <kamdinrao@gmail.com>
2023-02-27 17:47:35 +0530

    hj

--- dom2.js
+++ dom2.js
@@ -5,8 +5,9 @@ var itemList = document.getElementById('items');
 form.addEventListener('submit', addItem);
 // Delete event
 itemList.addEventListener('click', removeItem);
-
+itemList.addEventListener('click', editItem)
 //filter.addEventListener('keyup', filterItems);
+   
 
 function addItem(e){
     e.preventDefault();
@@ -16,7 +17,7 @@ function addItem(e){
     var newItem3 = document.getElementById('item3').value;
     var all = "Name:"+newItem1+",Emailid:"+newItem2+",Phonenumber:"+newItem3
     //add to local storage 
-    let user = localStorage.setItem(newItem2,all);
+    let user = localStorage.setItem(newItem2, all);
      
     // Create new li element
     var li = document.createElement('li');
@@ -28,31 +29,61 @@ function addItem(e){
     // li.appendChild(document.createTextNode(newItem3));
     // Create del button element
     var deleteBtn = document.createElement('button');
-   
+    let editBtn = document.createElement('button')
+    
     // Add classes to del button
     deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
-    
+    editBtn.className = 'btn btn-danger btn-sm float-right edit';
     // Append text node
     deleteBtn.appendChild(document.createTextNode('Delete'));
-
+    editBtn.appendChild(document.createTextNode('Edit'))
     // Append button to li
     li.appendChild(deleteBtn);
-    
+    li.appendChild(editBtn)
     // Append li to list
     itemList.appendChild(li);
+    
   }
+
+// function submit(){
+//   var newItem1 = document.getElementById('item1');
+//   var newItem2 = document.getElementById('item2');
+//   var newItem3 = document.getElementById('item3');
+
+//   newItem1.style.display = "none";
+//   newItem2.style.display = "none";
+//   newItem3.style.display = "none";
+// }
+
 function removeItem(e){
-    var newItem2 = document.getElementById('item2').value;
+    var  newItem2 = document.getElementById('item2').value;
     if(e.target.classList.contains('delete')){
       if(confirm('Are You Sure?')){
         var li = e.target.parentElement;
         itemList.removeChild(li);
-        let user =localStorage.removeItem(newItem2)
+        localStorage.removeItem(newItem2)
       }
     }
   }
 
+function editItem(e){
+  var newItem1 = document.getElementById('item1');
+  var newItem2 = document.getElementById('item2');
+  var newItem3 = document.getElementById('item3');
+    
+    if(e.target.classList.contains('edit')){
+      if(confirm('Are You Sure?')){
+        var li = e.target.parentElement;
+        itemList.removeChild(li);
+        localStorage.removeItem(newItem2)
 
+        newItem1.style.display = "block";
+        newItem2.style.display = "block";
+        newItem3.style.display = "block";
+        editItem.style.display = "none";
+      }
+    }
+}
 //   function filterItems(e){
 //     // convert text to lowercase
 //     var text = e.target.value.toLowerCase();
